28a484246b1a363d26a76af32fb5d797faf5c141
RunBloodz <ptq5vhjyhw@privaterelay.appleid.com>
2025-12-19 20:22:21 +0100

    Update macro.py

---
+++ macro.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+class MacroController:
+    def __init__(self):
+        self.running = False
+        self.thread = None
+
+    def start(self):
+        if self.running:
+            return
+        self.running = True
+        self.thread = threading.Thread(target=self.loop, daemon=True)
+        self.thread.start()
+
+    def stop(self):
+        self.running = False
+
+    def loop(self):
+        while self.running:
+            # SAFE PLACEHOLDER
+            print("Macro running...")
+            time.sleep(0.1)
+# macro placeholder
